8653d32896dde49b8d7a266c73cb11f8fde1a49d
374551703@qq.com <374551703@qq.com>
2018-11-08 20:45:25 +0800

    dd

--- src/Listeners/FileListener.php
+++ src/Listeners/FileListener.php
@@ -1,29 +1,14 @@
 <?php
-/** .-------------------------------------------------------------------
- * |      Site: www.hdcms.com
- * |      Date: 2018/6/28 下午8:07
- * |    Author: 向军大叔 <2300071698@qq.com>
- * '-------------------------------------------------------------------*/
 namespace Windmill\Upload\Listeners;
 use Windmill\Upload\Events\UploadEvent;
 
 class FileListener
 {
-    /**
-     * Create the event listener.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
-
     }
 
-    /**
-     * @param \App\Events\UploadEvent $event
-     *
-     * @return bool
-     */
     public function handle(UploadEvent $event)
     {
         if ( ! $request = $event->getRequest()) {
